3407decf2241695bca7ab3043958086ef77b784e
Ekaterina Verbitskaia <kajigor@gmail.com>
2020-10-21 18:25:20 +0300

    Computation of depth and width of a formula

--- experiments/propEval/helper.ml
+++ experiments/propEval/helper.ml
@@ -11,6 +11,9 @@ with show, gmap
 
 @type f = (f, Std.Nat.logic) fa logic with show, gmap
 
+@type ground_f = (ground_f, Std.Nat.ground) fa with show, gmap
+
+
 module F = Fmap2 (struct type ('a, 'b) t = ('a, 'b) fa let fmap f g x = gmap(fa) f g x end)
 
 let conj x y = inj @@ F.distrib (Conj (x, y))
@@ -18,21 +21,29 @@ let disj x y = inj @@ F.distrib (Disj (x, y))
 let var  x   = inj @@ F.distrib (Var x)
 let neg  x   = inj @@ F.distrib (Neg x)
 
-let max x y m =
+let max_nat x y m =
   ((Std.Nat.geo x y (!!true)) &&& (m === x)) ||| ((Std.Nat.leo x y (!!true)) &&& (m === y))
 
 
-let rec depth fm d =
+let rec depth_log fm d =
   (fresh (z) ((fm === var z) &&& (d === Std.Nat.zero))) |||
-  (fresh (x dx) ((fm === neg x) &&& (d === Std.Nat.succ dx) &&& (depth x dx))) |||
-  (fresh (x y dx dy m) ((fm === conj x y) &&& (depth x dx) &&& (depth y dy) &&& (max dx dy m) &&& (d === Std.Nat.succ m))) |||
-  (fresh (x y dx dy m) ((fm === disj x y) &&& (depth x dx) &&& (depth y dy) &&& (max dx dy m) &&& (d === Std.Nat.succ m)))
+  (fresh (x dx) ((fm === neg x) &&& (d === Std.Nat.succ dx) &&& (depth_log x dx))) |||
+  (fresh (x y dx dy m) ((fm === conj x y) &&& (depth_log x dx) &&& (depth_log y dy) &&& (max_nat dx dy m) &&& (d === Std.Nat.succ m))) |||
+  (fresh (x y dx dy m) ((fm === disj x y) &&& (depth_log x dx) &&& (depth_log y dy) &&& (max_nat dx dy m) &&& (d === Std.Nat.succ m)))
 
 
 let rec reify_f f = F.reify reify_f Std.Nat.reify f
 
 let var_to_string r = (show(logic) (show(bool))) (r#reify reify)
 let fm_to_string fm = (show(f)) (fm#reify reify_f)
+
+let project_4th_var n stream =
+  RStream.take ~n:n
+  @@ stream (fun s q r t -> (s, q, r, project t))
+
+let iter_formula_project n r f =
+  List.iter f
+  @@ project_4th_var n r
 
 let stream_4_vars_to_list n stream =
   RStream.take ~n:n
@@ -43,17 +54,42 @@ let iter_formula_3_vars n r f =
   @@ stream_4_vars_to_list n r
 
 
+let rec depth = function
+| Var x -> 1
+| Neg x -> 1 + (depth x)
+| Conj (x, y) -> 1 + (max (depth x) (depth y))
+| Disj (x, y) -> 1 + (max (depth x) (depth y))
+
+let rec branches = function
+| Var x -> 0
+| Neg x -> 1 + (branches x)
+| Conj (x, y) -> 2 + ((branches x) + (branches y))
+| Disj (x, y) -> 2 + ((branches x) + (branches y))
+
+let average_depth xs =
+  let depths = List.map (fun (_,_,_,x) -> depth x) xs in
+  let sum = List.fold_left (fun x y -> x + y) 0 depths in
+  float_of_int sum /. float_of_int (List.length depths)
+
+let average_branch xs =
+  let branchs = List.map (fun (_,_,_,x) -> branches x) xs in
+  let sum = List.fold_left (fun x y -> x + y) 0 branchs in
+  float_of_int sum /. float_of_int (List.length branchs)
+
 let run_formula n textRepr r =
-  Printf.printf "-----------------------------\n%s\n" textRepr;
+  let average = average_depth (RStream.take ~n:n @@ r (fun x y z t -> (x, y, z, project t))) in
+  Printf.printf "-----------------------------\nAverage Depth: %s\n%s\n" (string_of_float average) textRepr;
+
   (* Printf.printf "\n\nSize: %i\n\n" (List.length @@ RStream.take ~n:n @@ r (fun q r t fm -> (q, r, t, fm)))
    ; *)
-  iter_formula_3_vars n r
+   iter_formula_project n r
     ( fun (q, r, t, fm) ->
         Printf.printf "O:\t%s\tS(O):\t%s\tS(S(O)):\t%s\tFm:\t%s\n"
           (var_to_string q)
+          (* (show(bool) r) *)
           (var_to_string r)
           (var_to_string t)
-          (fm_to_string fm)
+          (Printf.sprintf "%s, Depth: %s" (show(ground_f) fm) (depth fm |> string_of_int))
     )
 
 let run_formula_f file =
@@ -65,6 +101,18 @@ let run_formula_f file =
           (var_to_string t)
           (fm_to_string fm)
     )
+
+
+let run_formula_f_proj file =
+  List.iter
+    ( fun (q, r, t, fm) ->
+        Printf.fprintf file "O:\t%s\tS(O):\t%s\tS(S(O)):\t%s\tFm:\t%s\n"
+          (var_to_string q)
+          (var_to_string r)
+          (var_to_string t)
+          (show(ground_f) fm)
+    )
+
 
 let run_time n text r =
   let t = Sys.time() in
@@ -118,25 +166,20 @@ let rec cross_diag xs =
   | [] -> []
   | x :: xs -> List.map (fun y -> (x, y)) xs @ cross_diag xs
 
-(* let max x y = if x > y then x else y
-
-let rec formula_depth = function
-| Var x -> 1
-| Neg x -> 1 + (formula_depth x)
-| Conj (x, y) -> 1 + (max (formula_depth x) (formula_depth y))
-| Disj (x, y) -> 1 + (max (formula_depth x) (formula_depth y))
-
-let average_depth xs =
-  let depths = List.map (fun (_,_,_,x) -> formula_depth (x#reify reify_f )) xs in
-  let sum = List.fold_left (fun x y -> x + y) 0 depths in
-  float_of_int sum /. float_of_int (List.length depths) *)
-
 let res_to_string (v1,v2,v3,fm) =
   Printf.sprintf  "%s, %s, %s, %s"
                   (var_to_string v1)
                   (var_to_string v2)
                   (var_to_string v3)
                   (fm_to_string fm)
+
+let res_to_string_4th_proj (v1, v2, v3, fm) =
+  Printf.sprintf  "%s, %s, %s, %s"
+                  (var_to_string v1)
+                  (var_to_string v2)
+                  (var_to_string v3)
+                  (show(ground_f) fm)
+
 
 let make_statistics oc lst =
   Printf.fprintf oc "Here lies statistics" ;
@@ -149,14 +192,18 @@ let make_statistics oc lst =
             then
               ()
             else
-              let file = Printf.sprintf "res/%s/%s.%s.log" goal_name name_x name_y in
+              let file = Printf.sprintf "res500/%s/%s.%s.log" goal_name name_x name_y in
+              let average_x = average_depth res_x in
+              let average_y = average_depth res_y in
+              let branches_x = average_branch res_x in
+              let branches_y = average_branch res_y in
               let oc = open_out file in
               (* Find a better way to compare formulas *)
               let (common, only_x, only_y, c, x, y) =
-                sameness (List.map res_to_string res_x)
-                         (List.map res_to_string res_y) in
-              (* Printf.fprintf oc "Common: %i\n%s: %i\n%s: %i\nAverageDepth_%s: %f\nAverageDepth_%s: %f" c name_x x name_y y name_x (average_depth res_x) name_y (average_depth res_y); *)
-              Printf.fprintf oc "Common: %i\n%s: %i\n%s: %i\n\nCommmon formulas:\n\n" c name_x x name_y y;
+                sameness (List.map res_to_string_4th_proj res_x)
+                         (List.map res_to_string_4th_proj res_y) in
+              Printf.fprintf oc "Common: %i\n%s: %i\n%s: %i\nAverage depth in %s: %f\nAverage depth in %s: %f\nAverage branch in %s: %f\nAverage branch in %s: %f\nCommmon formulas:\n\n" c name_x x name_y y name_x average_x name_y average_y name_x branches_x name_y branches_y;
+              (* Printf.fprintf oc "Common: %i\n%s: %i\n%s: %i\nCommmon formulas:\n\n" c name_x x name_y y ; *)
               List.iter (fun x -> Printf.fprintf oc "%s\n" x) common;
               Printf.fprintf oc "\nOnly in %s\n\n" name_x;
               List.iter (fun x -> Printf.fprintf oc "%s\n" x) only_x;
@@ -172,18 +219,18 @@ let make_statistics oc lst =
 let make_report lst =
   List.iter
     ( fun (num, goal_name, xs) ->
-      try Unix.mkdir (Printf.sprintf "res/%s" goal_name) 0o755
+      try Unix.mkdir (Printf.sprintf "res500/%s" goal_name) 0o755
       with _ -> () ;
 
       List.iter
         ( fun (name, results) ->
-          let file = Printf.sprintf "res/%s.%s.out" goal_name name in
+          let file = Printf.sprintf "res500/%s.%s.out" goal_name name in
           let oc = open_out file in
-          run_formula_f oc results;
+          run_formula_f_proj oc results;
           close_out oc
         ) xs
     ) lst
-  ; let file = "res/statistics.log" in
+  ; let file = "res500/statistics.log" in
   let oc = open_out file in
   make_statistics oc lst ;
   close_out oc
--- experiments/propEval/run.ml
+++ experiments/propEval/run.ml
@@ -9,7 +9,42 @@ let inputs =
   ; "   etalonLimited", EtalonLastPlainLimited.topLevel
   ; "        original", (fun x y z -> OriginalLastPlain.topLevel x y)
   ; "          etalon", (fun x y z -> EtalonLastPlain.topLevel x y)
+  ; "         perfect", (fun x y z -> PerfectLastPlain.topLevel x y)
+  (* ; "     etalonExtra", (fun x y z -> EtalonLastPlainExtra.topLevel x y) *)
   ]
+
+(* let _ =
+  run_formula 50 "Tmp" @@
+  run qrst (fun q r s fm -> OriginalLastPlainLimited.topLevel (ocanren {[q]}) fm two) *)
+
+(* let goals =
+  List.map
+    ( fun (num, vars, name) ->
+      ( num
+      , (fun eval ->
+          run qrst (fun q r s fm -> eval (vars q r s) fm two))
+      , name
+      )
+    )
+  [ (37  , (fun q r s -> ocanren {[q]}    ), "1v2d")
+  ; (530 , (fun q r s -> ocanren {[q;r]}  ), "2v2d")
+  (* ; (3015, (fun q r s -> ocanren {[q;r;s]}), "3v2d") *)
+  ]
+
+let mapped =
+  List.map
+    ( fun (num, goal, goalName) ->
+      ( num
+      , String.trim goalName
+      , List.map
+          ( fun (name, eval) ->
+            (String.trim name, goal eval |> project_4th_var num)
+          ) inputs
+      )
+    ) goals
+
+let _ =
+  make_report mapped *)
 
 
 let goals =
@@ -21,9 +56,10 @@ let goals =
       , name
       )
     )
-  [ (37  , (fun q r s -> ocanren {[q]}    ), "1v2d")
-  ; (530 , (fun q r s -> ocanren {[q;r]}  ), "2v2d")
-  ; (3015, (fun q r s -> ocanren {[q;r;s]}), "3v2d")
+  [
+    (* (500, (fun q r s -> ocanren {[q]}    ), "1v500") *)
+  ; (500, (fun q r s -> ocanren {[q;r]}  ), "2v500")
+  (* ; (1000, (fun q r s -> ocanren {[q;r;s]}), "3v1000") *)
   ]
 
 let mapped =
@@ -33,25 +69,24 @@ let mapped =
       , String.trim goalName
       , List.map
           ( fun (name, eval) ->
-            (String.trim name, goal eval |> stream_4_vars_to_list num)
-          ) inputs
+            (String.trim name, goal eval |> project_4th_var num)
+          ) (List.tl (List.tl inputs))
       )
     ) goals
 
 let _ =
   make_report mapped
 
-
-
 let runs =
   let iters = 4L in
   List.map (fun (num, vars, name) ->
     do_tables iters num (fun eval ->
-      run q (fun fm -> fresh (q r s) (eval (vars q r s) fm two))) inputs name
+      run q (fun fm -> fresh (q r s) (eval (vars q r s) fm two))) (List.tl (List.tl inputs)) name
   ) @@
-  [ (37  , (fun q r s -> ocanren {[q]}    ), "1v2d")
-  ; (530 , (fun q r s -> ocanren {[q;r]}  ), "2v2d")
-  ; (3015, (fun q r s -> ocanren {[q;r;s]}), "3v2d")
+  [
+    (* (500 , (fun q r s -> ocanren {[q]}    ), "1v2d") *)
+  ; (500 , (fun q r s -> ocanren {[q;r]}  ), "2v2d")
+  (* ; (3015, (fun q r s -> ocanren {[q;r;s]}), "3v2d") *)
   ]
 
 let _ =
